b543c23b7ad59f9142d45b0867fb918f69b1f42e
Najmul-Hasan-Sabbir <najmulsabbir2020@gmail.com>
2024-04-21 22:36:49 +0600

    second  commit

--- client/src/components/ProfileMenu/ProfileMenu.jsx
+++ client/src/components/ProfileMenu/ProfileMenu.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
-import {Avatar, Menu} from '@mantine/core'
+ import {Avatar, Menu} from '@mantine/core'
 import { useNavigate } from 'react-router-dom'
 const ProfileMenu = ({user, logout}) => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+
   return (
     <Menu>
         <Menu.Target>
             <Avatar src={user?.picture} alt='user image' radius={"xl"}/>
         </Menu.Target>
         <Menu.Dropdown>
-            {/* <Menu.Item onClick={()=> navigate("./favourites", {replace: true})}>
-                Favourites
-            </Menu.Item> */}
-<Menu.Item onClick={()=> navigate("./favourites", {replace: true})}>
+            <Menu.Item onClick={()=> navigate("./favourites", {replace: true})}>
                 Favourites
             </Menu.Item>
+
             <Menu.Item onClick={()=> navigate("./bookings", {replace: true})}>
                 Bookings
             </Menu.Item>
-
+   
+   
             <Menu.Item onClick={()=>{
                 localStorage.clear();
                 logout()
