d544f5fcc2caf5cdb536c4df9ceb881429eb4c21
Najmul-Hasan-Sabbir <najmulsabbir2020@gmail.com>
2024-04-21 21:55:46 +0600

    first commit

---
+++ client/src/main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Auth0Provider
+     domain="dev-oi3i2x5niozduspo.us.auth0.com"
+     clientId="vskt3ibVHlefwV1V8NxhjS3wY6SKhV8R"
+     authorizationParams={{
+      redirect_uri: "http://localhost:5173"
+     }}
+     audience="http://localhost:8000"
+     scope="openid profile email"
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
